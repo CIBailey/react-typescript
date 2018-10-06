4c18cf6ed1210b827f57d5c150652af3411a8e53
Nyura95 <taxisoopya@hotmail.fr>
2018-10-07 01:28:36 +0200

    pending

--- src/components/Button/__test__/Component.test.tsx
+++ src/components/Button/__test__/Component.test.tsx
@@ -1,18 +1,38 @@
 import * as React from 'react';
 import * as ReactTestRenderer from 'react-test-renderer';
 
+import * as enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
+
 // tslint:disable-next-line:import-name
 import Component from '../index';
 
+const Test = (
+  <Component color={'info'} onClick={() => console.log('ty')}>
+    <span>test</span>
+  </Component>
+);
+
 describe('BUTTON COMPONENT', () => {
-  const component: ReactTestRenderer.ReactTestRenderer = ReactTestRenderer.create(<Component color={'info'} />);
+  const component: ReactTestRenderer.ReactTestRenderer = ReactTestRenderer.create(Test);
 
   it('checking snapshot', () => {
     expect(component.toJSON()).toMatchSnapshot();
   });
 
-  it('test', () => {
-    const tree: unknown = component.toJSON();
-    expect(tree).toBeDefined();
+  it('Check if children is created', () => {
+    expect(enzyme.shallow(Test).contains(<span>test</span>)).toBeTruthy();
+  });
+
+  it('Test props', () => {
+    expect(enzyme.shallow(Test).prop('color')).toEqual('info');
+  });
+
+  it('simulate button', () => {
+    enzyme
+      .mount(Test)
+      .find('button')
+      .simulate('click');
   });
 });
